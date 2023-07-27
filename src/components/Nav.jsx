7f94d71a13c61be83a2046e90e582eda213d12f5
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-07-27 21:21:53 +0530

    Changed Structure of the project

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 function Nav() {
   const [show, handleShow] = useState(false);
   const navigate = useNavigate();
-  const transitionNavBar = () =>{
-    if(window.scrollY > 100){
-        handleShow(true);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
     }
-    else{
-        handleShow(false);
-    }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     window.addEventListener("scroll", transitionNavBar);
-    return ()=>{
-        window.removeEventListener('scroll', transitionNavBar)
-    }
-  }, [])
+    return () => {
+      window.removeEventListener("scroll", transitionNavBar);
+    };
+  }, []);
   return (
-    <div className = {`nav ${show?"nav_black":""} `}>
-        <div className="nav_content">
-            <img 
-            className = 'nav_logo'
-            onClick={()=>{
-              navigate('/');
-            }}
-            src="/images/misc/logo.svg"
-            alt="" 
-            />
-
-            <img 
-            onClick = {()=>{
-              navigate('/profile')
-            }}
-            className='nav_avatar'
-            src="/images/users/1.png" 
-            alt="" />
-        </div>
+    <div className={`nav ${show ? "nav_black" : ""}`}>
+      <div className="nav_content flex px-4">
+        <img
+          className="nav_logo"
+          onClick={() => {
+            navigate("/");
+          }}
+          src="/images/misc/logo.svg"
+          alt=""
+        />
+        <div className="text-white">Search...</div>
+        <img
+          onClick={() => {
+            navigate("/profile");
+          }}
+          className="nav_avatar"
+          src="/images/users/1.png"
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
