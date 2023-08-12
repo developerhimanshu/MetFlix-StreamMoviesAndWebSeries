1de70943daa344b89439498f8094eee53b3f3e0b
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-08-12 17:03:34 +0530

    Created Login, register and server using express, node and mongoDB

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const connectDb = require("./db/connect");
+const authRoute = require("./routes/auth.js");
+const port = process.env.PORT || 3001;
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handle");
 
-const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.use(express.static("../dist"));
+app.use("/api/v1/auth", authRoute);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-app.listen(port, (req, res) => {
-  console.log("Your app is listening on port " + port);
-});
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, (req, res) => {
+      console.log("Your app is listening on port " + port);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
