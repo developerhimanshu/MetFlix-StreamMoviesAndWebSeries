9376ca9132acb8f5278f6569c561f6058629d772
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-08-17 23:32:00 +0530

    Sidebar changes

--- src/pages/ProfileScreen.jsx
+++ src/pages/ProfileScreen.jsx
@@ -3,14 +3,14 @@ import { useAuth } from "../AuthContext";
 const ProfileScreen = () => {
   const { user, logout } = useAuth();
   return (
-    <div className="w-full">
+    <div className="w-full profileScreen">
       <div className="profileScreen_body">
         <h1>Edit Profile</h1>
         <div className="profileScreen_info">
           <img src="/images/users/1.png" alt="" />
 
           <div className="proflieScreen_details">
-            <h2>{user.user.name}</h2>
+            <h2>Welcome, {user.user.name}</h2>
             <div className="profileScreenPlans">
               <h3>Plans</h3>
               <button
